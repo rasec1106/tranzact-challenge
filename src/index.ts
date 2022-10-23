247f09ac08c5dd81d9280798f873f1addd7be8ec
rasec1106 <elch.herrera@gmail.com>
2022-10-23 01:45:05 -0500

    extra configurations for deploying

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import planRouter from './routes/plans'
 import { logger } from './middleware/logger'
+import cors from 'cors'
 
 const app = express()
+const PORT = process.env.PORT || 5000
+app.set('port', PORT)
 
 // parse form data
 app.use(express.urlencoded({ extended: false }))
@@ -10,6 +13,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 // extra middlewares
 app.use(express.static('public'))
+app.use(cors())
 app.use('/api', logger)
 // routing
 app.use('/api/plans', planRouter)
@@ -18,7 +22,6 @@ app.all('*', (_req, res) => {
   res.status(404).send('Resource not found')
 })
 
-const PORT = 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
